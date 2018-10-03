8c868ad11aff22c303df52188fa1f76804fb251f
ChloeHelen <cmingay@fastmail.fm>
2018-10-03 14:37:40 +0100

    Airport can land planes

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -10,4 +10,11 @@
        expect(airport.land('plane')).toEqual("plane landed");
      });
    });
+
+   describe("#takeOff", function() {
+     it("Should take off a plane", function() {
+       expect(airport.takeOff('plane')).toEqual("plane taken off");
+     });
+   });
+
  });
--- src/airport.js
+++ src/airport.js
@@ -3,3 +3,7 @@ function Airport() {}
 Airport.prototype.land = function(plane) {
   return plane + " landed";
 };
+
+Airport.prototype.takeOff = function(plane) {
+  return plane + " taken off";
+};
