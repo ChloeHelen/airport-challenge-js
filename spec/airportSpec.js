f567b492572f7610037afefd7fb2681df270f84a
ChloeHelen <cmingay@fastmail.fm>
2018-10-03 14:58:33 +0100

    Plane can't land when it's stormy

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -9,16 +9,17 @@
      it("Should land a plane", function() {
        expect(airport.land('plane')).toEqual("plane landed");
      });
+     it("Should not allow a plane land when the weather is stormy", function() {
+       expect(airport.land('plane', 'stormy')).toEqual("It's too stormy to land!");
+     });
    });
 
    describe("#takeOff", function() {
      it("Should take off a plane", function() {
-       expect(airport.takeOff('plane', 'sunny')).toEqual("plane taken off");
+       expect(airport.takeOff('plane')).toEqual("plane taken off");
      });
      it("Should not allow a plane to take off when the weather is stormy", function() {
-       expect(airport.takeOff('plane', 'stormy')).toEqual("It's too stormy!");
+       expect(airport.takeOff('plane', 'stormy')).toEqual("It's too stormy to take off!");
      });
    });
-
-
  });
--- src/airport.js
+++ src/airport.js
@@ -1,13 +1,17 @@
 function Airport() {}
 
-Airport.prototype.land = function(plane) {
-  return plane + " landed";
+Airport.prototype.land = function(plane, weather) {
+  if (weather === "stormy") {
+    return "It's too stormy to land!";
+  } else {
+    return plane + " landed";
+  }
 };
 
 Airport.prototype.takeOff = function(plane, weather) {
   if (weather === "stormy") {
-    return "It's too stormy!";
+    return "It's too stormy to take off!";
   } else {
-  return plane + " taken off";
+    return plane + " taken off";
   }
 };
